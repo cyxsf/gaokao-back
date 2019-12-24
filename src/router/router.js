02a4d300ea1161ddbc816bae934393f5c3c85138
cyxsf <870467987@qq.com>
2019-12-24 17:03:13 +0800

    first commit

---
+++ src/router/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/pages/home/home'
+import Login from '@/pages/other/login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }
+  ]
+})
